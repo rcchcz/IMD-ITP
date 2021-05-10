22728c4a0594d6916a9c77a9b830bdfbc27e407f
Rita Cruz <rcchcz@gmail.com>
2021-05-10 10:08:45 -0300

    Add files via upload

---
+++ oddValuesSum.c
@@ -0,0 +1,33 @@
+// This program receives 1 integer number n, then it receives
+// 2 integers numbers x and y and prints the sum of all odd
+// numbers between x and y. This happens n times. 
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+    {
+        int x, y;
+        int sum = 0;
+        scanf("%d", &x);
+        scanf("%d", &y);
+        y--;
+
+        while(y>x)
+        {
+            if(y%2 != 0) sum += y;
+            y--;
+        }
+
+        printf("%d \n", sum);
+        sum = 0;
+    }
+
+    return 0;
+}
